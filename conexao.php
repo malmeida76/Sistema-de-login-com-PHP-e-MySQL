fbb748bbf8c4aebf3bc5feb5ba3137b9db34b49d
Marcus Almeida <92253558+malmeida76@users.noreply.github.com>
2023-06-10 10:45:11 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 define('HOST', 'localhost'); // O VALOR DE HOST É O IP DO BANCO DE DADOS MYSQL
 define('USUARIO', 'root');
 define('SENHA', '');
-define('DB', 'login');
+define('DB', 'fm_afiliados_wpp');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Não foi possível conectar');
 ?>
